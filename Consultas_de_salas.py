a10524b91debf36a42495f4a4ff7e326f4f7a8f5
Hanakkyushi5 <113265426+Hanakkyushi5@users.noreply.github.com>
2022-11-02 14:00:25 -0600

    Update Consultas_de_salas.py

--- Consultas_de_salas.py
+++ Consultas_de_salas.py
@@ -1,26 +1,29 @@
-# Este deberia mostrar todo lo que hay en el select pero por fecha pero no consegui pasar el str a formato fecha
+# Este me da en teoria las que estan disponibles pero tuve que poner una columna en reservas llamada nombre porque no me dejaba unirla con la de salas, 
+# me salia que era ambiguo el nombre (id_sala) asi que puse la columna solo para probarlo, faltaria la restriccion de que no agarre los que ya tienen reserva pero 
+# no se del todo como podria hacerlo
 import datetime
 import sqlite3
 from sqlite3 import Error
 import sys
 
-fecha_consultar = input("Fecha que desea consultar (dd/mm/aaaa): ")
+fecha_consultar = input("Dime una fecha (dd/mm/aaaa): ")
 fecha_consultar = datetime.datetime.strptime(fecha_consultar, "%d/%m/%Y").date()
 
 try:
     with sqlite3.connect("evidencia3.db", detect_types = sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES) as conn:
         mi_cursor = conn.cursor()
-        criterios = {"fecha":fecha_consultar}
-        mi_cursor.execute("SELECT folio, fechar, id_cliente, id_sala, nombre_evento, id_turno FROM reservas WHERE fechar = :fecha;", criterios)
+        valores = {"fechar":fecha_consultar}
+        mi_cursor.execute("SELECT id_sala, nombre, nombret, fecha FROM reservas, turnos WHERE DATE(fecha) = :fechar;", valores)
         registros = mi_cursor.fetchall()
    
-        for folio, fechar, id_cliente, id_sala, nombre_evento, id_turno in registros:
-            print(f"folio = {folio}")
-            print(f"fechar = {fechar.date()}, tipo de dato {type(fechar)}")
-            print(f"nombre_evento = {nombre_evento}")
-            print(f"id_sala = {id_sala}")
-            print(f"id_turno = {id_turno}")
-            print(f"id_cliente = {id_cliente}\n")
+        if registros:
+            for id_sala, nombre, nombret, fecha in registros:
+                  print(f"ID_Sala = {id_sala}")
+                  print(f"Turno = {nombret}")
+                  print(f"Sala = {nombre}")
+                  print(f"Fecha = {fecha.date().strftime('%d/%m/%Y')}, tipo de dato {type(fecha)}\n")
+        else:
+            print(f"No se encontró salas disponibles asociadas con la fecha {fecha_consultar}")
         
 except sqlite3.Error as e:
     print (e)
