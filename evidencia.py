050209b76b4f813e79a647d38c040b3b206c1d85
Luis Hdz <luishernandez16141@gmail.com>
2022-11-02 11:58:56 -0600

    commit funciones para datos entero y de tipo fecha

--- evidencia.py
+++ evidencia.py
@@ -5,6 +5,18 @@ from sqlite3 import Error
 import openpyxl
 import os
 
+def puede_ser_tipo_fecha(fecha):
+    try:
+        return datetime.datetime.strptime(fecha, "%d/%m/%Y").date()
+    except Exception:
+        return False
+
+def puede_ser_int(valor):
+    try:
+        return int(valor)
+    except Exception:
+        return False
+
 if os.path.isfile("evidencia3.db"):
     print("\nSe detecto que hay datos previos.")
 else:
@@ -22,6 +34,8 @@ else:
         conn_creacion_tablas.close()
     except Error as e:
         print(e)
+
+consulta_reservaciones = []
 
 while True:
 
@@ -77,7 +91,75 @@ while True:
             opcion_menu_reportes = input("\nElija una opción: ")
 
             if opcion_menu_reportes.upper() == "A":
-                pass
+                while True:
+
+                    consulta_reservaciones.clear()
+
+                    fecha_a_consultar_capturada = input("\nFecha que desea consultar si hay reservaciones: ")
+
+                    if fecha_a_consultar_capturada.strip() == "":
+                        print("\nNo se puede omitir la fecha.")
+                        continue
+                    
+                    fecha_a_consultar = puede_ser_tipo_fecha(fecha_a_consultar_capturada)
+
+                    if fecha_a_consultar == False:
+                        print("\nNo es de tipo de fecha correcto.")
+                        continue
+
+                    try:
+                        with sqlite3.connect("evidencia3.db") as conn_reservas_reporte:
+                            mi_cursor = conn_reservas_reporte.cursor()
+                            fecha_ = fecha_a_consultar.strftime("%d/%m/%Y"),
+                            mi_cursor.execute(f"SELECT id_sala, id_cliente, nombre_evento ,id_turno FROM reservas WHERE fecha=?", fecha_)
+                            registros_reservas_reporte = mi_cursor.fetchall()
+                        conn_reservas_reporte.close()
+                    except Error as e:
+                        print(e)
+                    except Exception:
+                        print(f"\nSe produjo el siguiente error: {sys.exc_info()[0]}")
+
+                        
+
+                    for id_sala, id_cliente, nombre_evento, id_turno in registros_reservas_reporte:
+                        try:
+                            with sqlite3.connect("evidencia3.db") as conn_clientes_reporte:
+                                mi_cursor = conn_clientes_reporte.cursor()
+                                mi_cursor.execute(f"SELECT  nombre FROM clientes WHERE id_cliente={id_cliente}")
+                                cliente_nombre_reporte = mi_cursor.fetchall()
+                            conn_clientes_reporte.close()
+                        except Error as e:
+                            print(e)
+                        except Exception:
+                            print(f"\nSe produjo el siguiente error: {sys.exc_info()[0]}")
+
+                        try:
+                            with sqlite3.connect("evidencia3.db")as conn_turnos_reporte:
+                                mi_cursor = conn_turnos_reporte.cursor()
+                                mi_cursor.execute(f"SELECT nombre FROM turnos WHERE id_turno={id_turno}")
+                                turno_nombre_reporte = mi_cursor.fetchall()
+                            conn_turnos_reporte.close()
+                        except Error as e:
+                            print(e)
+                        except Exception:
+                            print(f"\nSe produjo el siguiente error: {sys.exc_info()[0]}")
+
+                        consulta_reservaciones.append((id_sala, cliente_nombre_reporte[0][0], nombre_evento, turno_nombre_reporte[0][0]))
+                        continue
+                            
+                    if len(consulta_reservaciones) == 0:
+                        print("\nNo hay reservaciones en esa fecha.")
+                        break
+
+                    print("*" * 100)
+                    print(f"{'REPORTE DE RESERVACIONES PARA EL DIA ' + fecha_a_consultar_capturada:^100}")
+                    print("*" * 100)
+                    print(f"{'SALA':<15}{'CLIENTE':<20}{'EVENTO':<50}TURNO")
+                    print("*" * 100)
+                    for datos in consulta_reservaciones:
+                        print(f"{datos[0]:<15}{datos[1]:<20}{datos[2]:<50}{datos[3]}")
+                    print("*" * 100)
+                    break
             elif opcion_menu_reportes.upper() == "B":
                 pass
             elif opcion_menu_reportes.upper() == "C":
